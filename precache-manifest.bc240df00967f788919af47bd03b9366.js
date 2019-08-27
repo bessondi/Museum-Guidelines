ba49994603abd56bf36c98319cad94bc74b5b9d9
Dmitry Bessonov <dmitrybessonov@icloud.com>
2019-08-27 18:31:07 +0300

    Updates

---
+++ precache-manifest.bc240df00967f788919af47bd03b9366.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2485d50d61345b0b41ada5fd68e51a7",
+    "url": "/Museum-Guidelines/index.html"
+  },
+  {
+    "revision": "9fb3b1b4f7eebd7ee6fe",
+    "url": "/Museum-Guidelines/static/css/main.0e3cd7a3.chunk.css"
+  },
+  {
+    "revision": "f8e6c00df13cbbc4c011",
+    "url": "/Museum-Guidelines/static/js/2.d927b9d1.chunk.js"
+  },
+  {
+    "revision": "9fb3b1b4f7eebd7ee6fe",
+    "url": "/Museum-Guidelines/static/js/main.a39e88fa.chunk.js"
+  },
+  {
+    "revision": "173345254118a2e11c3f",
+    "url": "/Museum-Guidelines/static/js/runtime~main.17e7a2ee.js"
+  },
+  {
+    "revision": "0b7c3d0fe2789dbcc2476b647e69ed13",
+    "url": "/Museum-Guidelines/static/media/1.0b7c3d0f.png"
+  },
+  {
+    "revision": "fa6d8f60cfd968ca7aae992593312f80",
+    "url": "/Museum-Guidelines/static/media/2.fa6d8f60.png"
+  },
+  {
+    "revision": "640723b957d44eeb29f49e7099946e58",
+    "url": "/Museum-Guidelines/static/media/3.640723b9.png"
+  },
+  {
+    "revision": "2dd67a7a3945f000efe6beae3b28a7c9",
+    "url": "/Museum-Guidelines/static/media/4.2dd67a7a.png"
+  },
+  {
+    "revision": "a63d4d6d5079c8d13cc248cce4f93a7d",
+    "url": "/Museum-Guidelines/static/media/404.a63d4d6d.svg"
+  },
+  {
+    "revision": "55fa53871c5baa28021109c7742750eb",
+    "url": "/Museum-Guidelines/static/media/Eva.55fa5387.jpg"
+  },
+  {
+    "revision": "cf93fd4a37ddde7632ccdcc723a2cd02",
+    "url": "/Museum-Guidelines/static/media/Fab.cf93fd4a.jpeg"
+  },
+  {
+    "revision": "fd50b0f417ffdc082de0a601b9d87fed",
+    "url": "/Museum-Guidelines/static/media/Gabsburg.fd50b0f4.jpeg"
+  },
+  {
+    "revision": "cbb7cfe5c92f63a7a699e09be0fb6e06",
+    "url": "/Museum-Guidelines/static/media/McCartey.cbb7cfe5.jpg"
+  },
+  {
+    "revision": "a958dd9e11b1b8f1940629221c38ac03",
+    "url": "/Museum-Guidelines/static/media/Munt.a958dd9e.jpeg"
+  },
+  {
+    "revision": "2e207889bff4428e8f92cc7d6e96900a",
+    "url": "/Museum-Guidelines/static/media/Scurlov.2e207889.jpg"
+  },
+  {
+    "revision": "463298714bb46fea5c4cb58f569767a1",
+    "url": "/Museum-Guidelines/static/media/SegoeUI.46329871.ttf"
+  },
+  {
+    "revision": "daab2944b2fe8f6eecac449ce300005c",
+    "url": "/Museum-Guidelines/static/media/Shaffer.daab2944.jpg"
+  },
+  {
+    "revision": "b403a3408585a0ae472ced471d6ea5dd",
+    "url": "/Museum-Guidelines/static/media/Solod.b403a340.jpg"
+  },
+  {
+    "revision": "14e5b9184a74f6fa07aec1580e3d6b80",
+    "url": "/Museum-Guidelines/static/media/Ulander.14e5b918.jpeg"
+  },
+  {
+    "revision": "63fbbcb927dde743528bf8fd37dd308a",
+    "url": "/Museum-Guidelines/static/media/arrow-down.63fbbcb9.svg"
+  },
+  {
+    "revision": "887382bc39cb1ab315bbc94a132f6c99",
+    "url": "/Museum-Guidelines/static/media/bg.887382bc.svg"
+  },
+  {
+    "revision": "285fe0d3195497c409c5f6391a5b4f6d",
+    "url": "/Museum-Guidelines/static/media/cafe.285fe0d3.jpeg"
+  },
+  {
+    "revision": "b09f156437572b24d19ec13a5cd7a1ba",
+    "url": "/Museum-Guidelines/static/media/fabergeLogo.b09f1564.svg"
+  },
+  {
+    "revision": "1c286f3b9e5012455f28295bc911d109",
+    "url": "/Museum-Guidelines/static/media/fabergeLogo2.1c286f3b.svg"
+  },
+  {
+    "revision": "04445a76a433b1cfa4fc4035340ffd99",
+    "url": "/Museum-Guidelines/static/media/facebook-logo.04445a76.svg"
+  },
+  {
+    "revision": "8b681c5eb9a570923e06d8d76116277a",
+    "url": "/Museum-Guidelines/static/media/instagram.8b681c5e.svg"
+  },
+  {
+    "revision": "f278545da5b57b070c023a8385397300",
+    "url": "/Museum-Guidelines/static/media/kombat.f278545d.jpeg"
+  },
+  {
+    "revision": "b0aa36d00d135590ef446abd28b82108",
+    "url": "/Museum-Guidelines/static/media/linkOfTimes.b0aa36d0.jpg"
+  },
+  {
+    "revision": "422f7097963ec0bca3ac70cd74f0764f",
+    "url": "/Museum-Guidelines/static/media/map1.422f7097.png"
+  },
+  {
+    "revision": "e24e68e7bd3e1e8215e91c8b3692eaad",
+    "url": "/Museum-Guidelines/static/media/map2.e24e68e7.png"
+  },
+  {
+    "revision": "77eab7dae53c287dd5c0a6e5f561762b",
+    "url": "/Museum-Guidelines/static/media/soundcloud-logo.77eab7da.svg"
+  },
+  {
+    "revision": "4aed88679e4327aa1278236681456c9b",
+    "url": "/Museum-Guidelines/static/media/team.4aed8867.jpeg"
+  },
+  {
+    "revision": "2bd73473817341249fea394e151602b6",
+    "url": "/Museum-Guidelines/static/media/vk-social-logotype.2bd73473.svg"
+  }
+]);
